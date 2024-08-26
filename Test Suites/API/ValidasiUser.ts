98d4f2479622babf082de74589a39a4d320ad677
hyper <hyper@DESKTOP-LJ32IRD>
2024-08-26 14:29:42 +0700

    Test Katalon API DOT

---
+++ Test Suites/API/ValidasiUser.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ValidasiUser</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>059220fc-ce81-4b3b-a913-5daccc1e0be1</testSuiteGuid>
+   <testCaseLink>
+      <guid>895cb3d4-df86-4fd2-9566-f30e486c1828</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/Validasi User</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
